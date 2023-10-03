fe3828db33f10cc4a8e002c58e921941b27c699c
Daniel Tan <danieltan380@gmail.com>
2023-10-03 13:05:12 -0700

    Added comments explaining details of each route

--- express_server.js
+++ express_server.js
@@ -12,7 +12,7 @@ const urlDatabase = {
 };
 
 const generateRandomString = function() {
-    const result = Math.random().toString(36).slice(7);
+    const result = Math.random().toString(36).slice(7); //Function to generate a random 6 character alpha-numeric string.
     return result;
 }
 
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
+app.get("/urls.json", (req, res) => {       //Route to /urls.json shows all urls in urlDatabase.
     res.json(urlDatabase);
 });
 
@@ -29,28 +29,31 @@ app.get("/hello", (req, res) => {
   });
 
 app.get("/urls", (req, res) => {
-    const templateVars = {urls: urlDatabase };
-    res.render("urls_index", templateVars);
-});
+    const templateVars = {urls: urlDatabase };  //Route to /urls shows all urls in urlDatabase, but formatted with html 
+    res.render("urls_index", templateVars);     //from "urls_index.ejs". templeVars has been defined so the ejs file can access
+});                                             //those variabls.
 
-app.get("/urls/new", (req, res) => {
+app.get("/urls/new", (req, res) => {            //Route to /urls/new for the page for creating new short urls.
     res.render("urls_new");
 });
 
-app.post("/urls", (req, res) => {
-    const randomString = generateRandomString();
-    urlDatabase[randomString] = req.body.longURL;
+app.post("/urls", (req, res) => {                   //Route after the POST submission of the new short url. User gets redirected to
+    const randomString = generateRandomString();    // /urls/randomString which will show the long url entered and the 6 character
+    urlDatabase[randomString] = req.body.longURL;   //alpha-numeric string.
     console.log(urlDatabase);
     res.redirect(`/urls/${randomString}`);
 });
 
-app.get("/urls/:id", (req, res) => {
-    const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
+app.get("/urls/:id", (req, res) => {            //Route to /urls/:id, ":id" being a place holder for Express to see what path
+    const templateVars = {                      //matches this pattern.
+        id: req.params.id, 
+        longURL: urlDatabase[req.params.id]
+    };
     res.render("urls_show", templateVars);
 })
 
-app.get("/u/:id", (req, res) => {
-    const longURL = urlDatabase[req.params.id];
+app.get("/u/:id", (req, res) => {               //Route to /u/:id which will redirect the user to the long url by using the short
+    const longURL = urlDatabase[req.params.id]; //url.
     console.log(req.params.id);
     res.redirect(longURL);
 });
